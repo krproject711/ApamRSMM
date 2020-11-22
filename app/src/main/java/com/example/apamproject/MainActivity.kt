d82b5fbd99fd0e5d9388f84419ea6b59c448b5fd
rvnltf <aldirivan56@gmail.com>
2020-11-22 09:50:03 +0700

    add Login Screen

--- app/src/main/java/com/example/apamproject/MainActivity.kt
+++ app/src/main/java/com/example/apamproject/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.apamproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_login.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome_screen)
+
+        val button = findViewById<Button>(R.id.masuk_button)
+        button.setOnClickListener{
+            val intent = Intent(this, login::class.java)
+            startActivity(Intent())
+        }
     }
 }
