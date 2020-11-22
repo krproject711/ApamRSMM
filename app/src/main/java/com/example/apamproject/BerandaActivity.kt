8a587fdb4cb66742491d5aa7194d11675eae2187
rvnltf <aldirivan56@gmail.com>
2020-11-22 22:45:19 +0700

    Pembaharuan Beranda

--- app/src/main/java/com/example/apamproject/BerandaActivity.kt
+++ app/src/main/java/com/example/apamproject/BerandaActivity.kt
@@ -3,27 +3,21 @@ package com.example.apamproject
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.apamproject.ui.dashboard.DashboardFragment
+import com.example.apamproject.ui.home.HomeFragment
+import com.example.apamproject.ui.notifications.NotificationsFragment
 
 class BerandaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_beranda)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
+
 }
