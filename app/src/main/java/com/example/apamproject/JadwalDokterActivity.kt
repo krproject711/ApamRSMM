7ee10770d022eebca49ded420de82d1773a12f35
rvnltf <aldirivan56@gmail.com>
2020-11-24 15:09:42 +0700

    Penambahan ignore view binding

---
+++ app/src/main/java/com/example/apamproject/JadwalDokterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.apamproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JadwalDokterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jadwal_dokter)
+    }
+}
