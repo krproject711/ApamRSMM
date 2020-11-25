196a9fbce849fa67f070f2b6186fb52c9b721e93
rvnltf <aldirivan56@gmail.com>
2020-11-25 15:25:21 +0700

    Penambahan View Jadwal Operasi

--- app/src/main/java/com/example/apamproject/JadwalOperasiActivity.kt
+++ app/src/main/java/com/example/apamproject/JadwalOperasiActivity.kt
@@ -3,6 +3,7 @@ package com.example.apamproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.apamproject.databinding.ActivityJadwalOperasiBinding
+import kotlinx.android.synthetic.main.activity_jadwal_operasi.*
 
 class JadwalOperasiActivity : AppCompatActivity() {
     private lateinit var binding: ActivityJadwalOperasiBinding
@@ -12,5 +13,8 @@ class JadwalOperasiActivity : AppCompatActivity() {
         binding = ActivityJadwalOperasiBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.viewpagerMain.adapter = MyPagerAdapter(supportFragmentManager)
+        binding.tabsMain.setupWithViewPager(viewpager_main)
     }
 }
